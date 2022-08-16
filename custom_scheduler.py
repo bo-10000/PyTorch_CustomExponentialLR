03405b95dec9c9e6f14da62f1b68e27c13ee95f6
Bomin Kim <seot100@naver.com>
2022-08-16 13:57:42 +0900

    add argument max_lr_decay

--- custom_scheduler.py
+++ custom_scheduler.py
@@ -14,6 +14,7 @@ class CustomExponentialLR(_LRScheduler):
             and some scaling of the amplitude; therefore
             max_lr may not actually be reached depending on
             scaling function.
+        max_lr_decay (float): Multiplicative factor of max_lr.
         step_size_up (int): Number of training iterations in the
             increasing half of a cycle. Default: 2000
         step_size_down (int): Number of training iterations in the
@@ -28,7 +29,7 @@ class CustomExponentialLR(_LRScheduler):
         verbose (bool): If ``True``, prints a message to stdout for
             each update. Default: ``False``.
     """
-    def __init__(self, optimizer, base_lr, max_lr, step_size_down=0, step_size_up=50, last_epoch=-1, verbose=False):
+    def __init__(self, optimizer, base_lr, max_lr, max_lr_decay=1., step_size_down=0, step_size_up=50, last_epoch=-1, verbose=False):
         self.optimizer = optimizer
         base_lrs = self._format_param('base_lr', optimizer, base_lr)
         if last_epoch == -1:
@@ -36,6 +37,7 @@ class CustomExponentialLR(_LRScheduler):
                 group['lr'] = lr
 
         self.max_lrs = self._format_param('max_lr', optimizer, max_lr)
+        self.max_lr_decay = max_lr_decay
 
         self.step_size_up = float(step_size_up)
         self.step_size_down = float(step_size_down) if step_size_down is not None else step_size_up
@@ -59,9 +61,11 @@ class CustomExponentialLR(_LRScheduler):
     def get_lr(self):
         
         cycle_epoch = self.last_epoch % self.total_size
+        cycle = self.last_epoch // self.total_size
         
         lrs = []
         for base_lr, max_lr in zip(self.base_lrs, self.max_lrs):
+            max_lr = max_lr * (self.max_lr_decay**cycle)
             if cycle_epoch < self.step_size_up:
                 lr = (max_lr - base_lr) / self.step_size_up * cycle_epoch + base_lr
             else:
